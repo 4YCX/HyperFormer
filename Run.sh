5a0ff5322dc0a4fe3067d15441691193bb8a1573
XCharlieY <Charlie050805@outlook.com>
2026-01-09 21:52:39 +0800

    Add files via upload

---
+++ Run.sh
@@ -0,0 +1,9 @@
+python train.py \
+    --dataset Berlin \
+    --model JViT \
+    --patch_size 7 \
+    --epoch 150 \
+    --lr 5e-3 \
+    --batch_size 256 \
+    --cuda 0 \
+    --flip_augmentation 
